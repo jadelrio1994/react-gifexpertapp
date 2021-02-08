f653a7f4939067773ab946f31b08dc9511da0303
Pikin <jadelrio1994@gmail.com>
2021-02-08 16:35:22 +0100

    first commit

---
+++ src/hooks/useFetchGif.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { getGifs } from "../Helpers/getGifs";
+
+export const useFetchGif = ( category ) => {
+  
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+
+    getGifs( category )
+        .then( imgs => {
+
+          setTimeout(() => {
+
+            setState({
+              data: imgs,
+              loading: false,
+            });
+
+          }, 3000)
+        })
+
+  }, [ category ]) 
+
+  return state;
+}
